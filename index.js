9da174424fa987741bf79e58958b557ede6d7bd8
Imran-72 <petrow318@gmail.com>
2022-02-27 20:17:03 +0300

    Асинхронное удаление элементов

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-const http = require('http');
+const express = require('express');
 const chalk = require('chalk');
+const path = require('path');
+const { addNote, getNotes, removeNote } = require('./notes.controller');
 
 const PORT = 3000;
+const app = express();
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-const server = http.createServer((req, res) => {
-  console.log('Request-method', req.method);
-  console.log('Request-url', req.url);
-  res.end('Hello from server!');
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'pages');
+app.get('/', async (req, res) => {
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false,
+  });
 });
 
-server.listen(PORT, () => {
+app.post('/', async (req, res) => {
+  const title = req.body.title;
+  await addNote(title);
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: true,
+  });
+});
+
+app.delete('/:id', async (req, res) => {
+  await removeNote(req.params.id);
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false,
+  });
+});
+
+app.listen(PORT, () => {
   console.log(chalk.greenBright(`Server has been started on port ${PORT}...`));
 });
